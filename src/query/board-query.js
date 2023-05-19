9df1b9e45cea3f76401926001ead1677ab23162a
Taehoon Kim <twodf78@naver.com>
2023-05-20 02:31:51 +0900

    query 변경

--- src/query/board-query.js
+++ src/query/board-query.js
@@ -1,7 +1,7 @@
 const query = {
   // getContent: "SELECT ? FROM anAlyst.analyst WHERE class = ? AND gender =?",
-  getTokenInfo: "SELECT * FROM picket.user WHERE userAddress = ?", //사용자id가 가지고 있는 피드정보를 return
-  getStatus: "SELECT * FROM picket.user", //사용자id가 가지고 있는 태그정보를 return
+  getTokenInfo: "SELECT * FROM picket.token WHERE userAddress = ?", //사용자id가 가지고 있는 피드정보를 return
+  getStatus: "SELECT * FROM picket.token", //사용자id가 가지고 있는 태그정보를 return
 };
 
 export default query;
