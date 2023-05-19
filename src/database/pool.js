0369ed686e33ea61c2a23f84169a1681300993c4
Taehoon Kim <twodf78@naver.com>
2023-05-19 23:30:32 +0900

    host 변경

--- src/database/pool.js
+++ src/database/pool.js
@@ -2,7 +2,7 @@ import { createPool } from "mysql2";
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: "localhost",
+  host: "127.0.0.1",
   port: 3306,
   user: "root",
   password: "rmsdnjsWkd@@1212", //mysql 설치시 설정했던 비번
