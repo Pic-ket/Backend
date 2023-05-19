9d80d4bb224b6b25b1653c45f28479fb8dbc395d
Taehoon Kim <twodf78@naver.com>
2023-05-20 02:30:41 +0900

    api 일부 수정

--- src/controller/board-controller.js
+++ src/controller/board-controller.js
@@ -4,7 +4,7 @@ import service from "../service/board-service.js";
 const router = express.Router();
 
 const params = {
-  tokenId: "",
+  userAddress: "",
 };
 
 const header = {
@@ -22,23 +22,24 @@ const host = [
 ];
 console.log("isReached??");
 // /를 가지고 있고,
-router.get("/status", async (req, res, next) => {
+
+router.get("/tokenInfo", async (req, res, next) => {
   console.log(req.headers.origin);
   //   if (host.includes(req.headers.origin))
   //     header["Access-Control-Allow-Origin"] = req.headers.origin;
 
-  params.tokenId = req.query.tokenId;
-  let rows = await service.getStatus(params, res, next); //service모듈의 getFeed함수를 이용해서 DB쿼리날린후, 정보를 가져온다
+  params.userAddress = req.query.userAddress;
+  let rows = await service.getTokenInfo(params, res, next); //service모듈의 getFeed함수를 이용해서 DB쿼리날린후, 정보를 가져온다
   return res.header(header).json(rows); //그걸 json으로 ~, response를 보낸다.
 });
 
-router.get("/tokenInfo", async (req, res, next) => {
+router.get("/status", async (req, res, next) => {
   console.log(req.headers.origin);
   //   if (host.includes(req.headers.origin))
   //     header["Access-Control-Allow-Origin"] = req.headers.origin; //Q.이게 뭔지 파악
 
   //   params.id = req.query.id;
-  let rows = await service.getTokenInfo(params, res, next);
+  let rows = await service.getStatus(params, res, next);
   return res.header(header).json(rows);
 });
 
--- src/service/board-service.js
+++ src/service/board-service.js
@@ -2,16 +2,16 @@ import query from "../query/board-query.js";
 import pool from "../database/pool.js";
 
 const service = {
-  getStatus: async (req, res, next) => {
+  getTokenInfo: async (req, res, next) => {
     console.log("isReached???");
 
-    let result = await pool.query(query.getStatus, [req.tokenId]);
+    let result = await pool.query(query.getTokenInfo, [req.userAddress]);
     console.log(result[0]);
     return result[0];
   },
 
-  getTokenInfo: async (req, res, next) => {
-    let result = await pool.query(query.getTokenInfo);
+  getStatus: async (req, res, next) => {
+    let result = await pool.query(query.getStatus);
     console.log(result[0]);
     return result[0];
   },
